105be54415608703f9482038e8f7130ec6ab686c
Lynne <134291887+julyando@users.noreply.github.com>
2023-05-22 19:54:47 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+class Hewan{
+    String lari;
+    String jalan;
+    String makan;}
+public class Main{
+public static void main(String[] args) throws Exception {
+    Hewan koecheng1 = new Hewan();
+        Koecheng1.lari = "Berlari dengan sangat cepat";
+        Koecheng1.jalan = "Berjalan sambil melompat";
+        Koecheng1.makan = "Makan ikan dengan menggunakan mulutnya";
+        System.out.println(Koecheng1.lari);
+        System.out.println(Koecheng1.jalan);
+        System.out.println(Koecheng1.makan);}}
