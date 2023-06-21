acd5e3db4f67de78bffee0c49ea62d7cf56d9c54
Serenity <134291887+Rest-u@users.noreply.github.com>
2023-06-21 13:20:30 +0700

    Update MainKinetic.java

--- Encaptulation/LiveCoding/MainKinetic.java
+++ Encaptulation/LiveCoding/MainKinetic.java
@@ -8,36 +8,60 @@ package Encaptulation.LiveCoding;
  *
  * @author Rest.u 22104410119
  */
-public class MainKinetic {
-    double massa;
-    double EK;
-    double kecepatan = Math.pow(2, 2);
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Encaptulation.LiveCoding;
 
+/**
+ *
+ * @author Rest.u 22104410119
+ */
+import java.util.Scanner;
+
+public class MainKinetic {
+    double massa, kecepatan;
+    
     public double getMassa() {
-        return massa;}
+        return massa;
+    }
 
     public void setMassa(double massa) {
-        this.massa = massa;}
-
-    public double getEK() {
-        return EK = 0.5 * massa * kecepatan;}
-
-    public void setEK(double EK) {
-        this.EK = EK;}
+        this.massa = massa;
+    }
 
     public double getKecepatan() {
-        return kecepatan;}
+        return kecepatan;
+    }
 
     public void setKecepatan(double kecepatan) {
-        this.kecepatan = kecepatan;}
+        this.kecepatan = kecepatan;
+    }
+    
+    public double EK(){
+    double energiKinetik = 0.5 * massa * Math.pow(kecepatan, 2);
+    return energiKinetik;
+}
     
     public static void main(String[] args) {
-    MainKinetic kinetic = new MainKinetic();
-    kinetic.setMassa(20);
-        System.out.println("diket : ");
-        System.out.println("kecepatan = 2 m/s ");
-        System.out.println("massa = 20 kg ");
-        System.out.println("ditanya = Energi Kinetic = 0.5 * m * v = ? ");
-    System.out.println("hasilnya adalah : " + kinetic.getEK());
+        MainKinetic kinetic = new MainKinetic();
+        Scanner scanner = new Scanner(System.in);
+            System.out.println("Ditanya =");
+            System.out.println("EK = 0.5 * m * v = ?");
+            System.out.println("Diketahui= ");
+            
+            System.out.println("massa benda (kg)= ");
+            double massa = scanner.nextDouble();
+            kinetic.setMassa(massa);
+            
+            System.out.println("kecepatan benda (m/s) = ");
+            double kecepatan = scanner.nextDouble();
+            kinetic.setKecepatan(kecepatan);
+            
+            double EK = kinetic.EK();
+            
+            System.out.println("Jawaban = ");
+            System.out.println("Energi kinetik : "+ EK +" Joule");
+        }
     }
-}
